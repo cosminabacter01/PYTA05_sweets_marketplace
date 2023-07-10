543a4cc84bde82dd67f0a26ef05a3f81f0c1e511
cosminadudas <cosmina.dudas@yahoo.ro>
2023-07-10 20:47:16 +0300

    added first route implementation and base template, home template (w.i.p) and navbar

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+Entrypoint-ul in aplicatia noastra web
+dezvoltata folosind framework-ul Flask.
+
+pip install flask
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7000)
